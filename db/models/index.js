b1e79ad4d757c7b3e317bb6c6350e488c0b82191
SamirIngley <samir.ingle7@gmail.com>
2020-06-11 15:48:19 -0700

    sequelize added, code to test if connection is live

--- db/models/index.js
+++ db/models/index.js
@@ -8,6 +8,15 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
+// Test connection is live
+sequelize.authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
