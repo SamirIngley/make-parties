d42c765df0ef722a986fae7589742dfacedae6f9
SamirIngley <samir.ingle7@gmail.com>
2020-06-11 22:31:26 -0700

    pages not loading, committing to revert back

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const exphbs = require('express-handlebars');
 
 // Initialize Body-Parser
 const bodyParser = require('body-parser');
+// must come after const app=express() and before routes
 app.use(bodyParser.urlencoded({ extended: true}));
 
 // requiring the models
@@ -27,21 +28,24 @@ var events = [
   { title: "I am your third event", desc: "A great event that is super fun to look at and good", author: "Anne", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
 ]
 
-// INDEX
+// Index
 app.get('/', (req, res) => {
-  res.render('events-index', { events: events });
+  models.Event.findAll({ order: [['createdAt', 'DESC']] }).then(events => {
+    res.render('events-index', { events: events });
+  })
 })
 
 app.get('/events/new', (req, res) => {
     res.render('events-new', {});
 })
 
+// CREATE
 app.post('/events', (req, res) => {
-    models.Event.create(req.body).then(event => {
-        res.redirect('/');
-    }).catch((err) => {
-        console.log(err)
-    });
+  models.Event.create(req.body).then(event => {
+    res.redirect(`/`);
+  }).catch((err) => {
+    console.log(err)
+  });
 })
 
 
